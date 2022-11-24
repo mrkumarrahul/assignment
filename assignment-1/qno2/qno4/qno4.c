3c4e181804f2a6fb8d0523b564a9e9ca3ec7f5d4
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2022-11-24 08:53:56 +0530

    v1.0

---
+++ assignment-1/qno2/qno4/qno4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+ float area,radius;
+ printf("enter radius to calculate the area of the circle");
+ scanf("%f",&radius);
+ area=3.14*radius*radius;
+ printf("The area of the circle is %f",area);
+ return 0;
+}
---
+++ assignment-1/qno2/qno4/qno5/qno5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+int x;
+x=printf("Rahul");
+printf("%d",x);
+return 0;
+}
